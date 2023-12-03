9eb28924f3b6683db8775316088dc94b8350aca4
Sky <sahnivyom@gmail.com>
2023-12-03 14:52:07 +0530

    Useriddisplay

--- app.py
+++ app.py
@@ -20,8 +20,11 @@ def route_add_user(username, email):
 
 @app.route('/users', methods=['GET'])
 def route_get_users():
+    # This function fetches users from the database and constructs a list of dictionaries
+    # with both the user's ID and username before returning it as a JSON response.
     users = get_users()
-    return jsonify({'users': [user.username for user in users]})
+    users_list = [{'id': user.id, 'username': user.username} for user in users]
+    return jsonify({'users': users_list})
 
 @app.route('/update_user/<int:user_id>/<username>/<email>', methods=['PUT'])
 def route_update_user(user_id, username, email):
@@ -34,7 +37,6 @@ def route_delete_user(user_id):
     return jsonify({'message': 'User deleted' if result else 'User not found'})
 
 # CRUD routes for Word
-# Add a new word
 @app.route('/add_word', methods=['POST'])
 def route_add_word():
     data = request.json
@@ -43,14 +45,12 @@ def route_add_word():
     add_word(japanese, english)
     return jsonify({'message': 'Word added'})
 
-# Get all words
 @app.route('/words', methods=['GET'])
 def route_get_words():
     words = get_words()
     words_list = [{'id': word.id, 'japanese': word.japanese, 'english': word.english} for word in words]
     return jsonify({'words': words_list})
 
-# Update a word
 @app.route('/update_word/<int:word_id>', methods=['PUT'])
 def route_update_word(word_id):
     data = request.json
@@ -59,16 +59,12 @@ def route_update_word(word_id):
     result = update_word(word_id, new_japanese, new_english)
     return jsonify({'message': 'Word updated' if result else 'Word not found'})
 
-# Delete a word
 @app.route('/delete_word/<int:word_id>', methods=['DELETE'])
 def route_delete_word(word_id):
     result = delete_word(word_id)
     return jsonify({'message': 'Word deleted' if result else 'Word not found'})
 
-
-
 # CRUD routes for Tag
-# Add a new tag
 @app.route('/add_tag', methods=['POST'])
 def route_add_tag():
     data = request.json
@@ -76,14 +72,12 @@ def route_add_tag():
     add_tag(name)
     return jsonify({'message': 'Tag added'})
 
-# Get all tags
 @app.route('/tags', methods=['GET'])
 def route_get_tags():
     tags = get_tags()
     tags_list = [{'id': tag.id, 'name': tag.name} for tag in tags]
     return jsonify({'tags': tags_list})
 
-# Update a tag
 @app.route('/update_tag/<int:tag_id>', methods=['PUT'])
 def route_update_tag(tag_id):
     data = request.json
@@ -91,13 +85,10 @@ def route_update_tag(tag_id):
     result = update_tag(tag_id, new_name)
     return jsonify({'message': 'Tag updated' if result else 'Tag not found'})
 
-# Delete a tag
 @app.route('/delete_tag/<int:tag_id>', methods=['DELETE'])
 def route_delete_tag(tag_id):
     result = delete_tag(tag_id)
     return jsonify({'message': 'Tag deleted' if result else 'Tag not found'})
-
-
 
 @app.cli.command('create_db')
 def create_db():
