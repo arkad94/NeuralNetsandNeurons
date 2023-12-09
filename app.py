af50c3a2013c8380da49240c7fbf050525452d33
Sky <sahnivyom@gmail.com>
2023-12-09 23:53:48 +0530

    app.pyupdate

--- app.py
+++ app.py
@@ -103,4 +103,5 @@ def create_db():
     print("Database tables created.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
