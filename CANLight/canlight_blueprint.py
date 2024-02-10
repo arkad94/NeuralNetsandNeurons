e35a0fa4314789a8626c07582a2a51854dfbe96e
Sky <sahnivyom@gmail.com>
2024-02-10 22:44:24 +0530

    CANLightbtntst

--- CANLight/canlight_blueprint.py
+++ CANLight/canlight_blueprint.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 canlight_blueprint = Blueprint('canlight', __name__, template_folder='CANLight')
 
 @canlight_blueprint.route('/canlight')
 def canlight():
     # Placeholder response, you can later update it to render a template
-    return "CANLight page under construction"
+    return render_template('canlight.html')
